5486b759d35d02c9402a5ffe095bb39f47418fe3
alessia <alessia.cerami@studio.unibo.it>
2019-11-19 18:11:24 +0100

    changes in nav + added homepage

--- frontend/src/components/Navbar/Navbar.vue
+++ frontend/src/components/Navbar/Navbar.vue
@@ -1,34 +1,33 @@
 <template>
-    <div class="nav-bar">
+    <div>
         <b-navbar toggleable="lg" type="dark" variant="info">
-            <router-link class="navbar-brand" to="/">Eco-assistant</router-link>
-            <b-nav-toggle target="nav_collapse"></b-nav-toggle>
-            <b-collapse is-nav id="nav_collapse">
+        <b-navbar-brand class="home" to="/">
+          <img src="../../assets/ecology_f.png">{{navhome}}
+        </b-navbar-brand>
 
-                <b-nav is-nav-bar>
-                    <router-link to="home">Informazioni prodotto</router-link>
-                    <router-link to="about" v-if="isLogged === true"> About</router-link>
-                </b-nav>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-                <b-nav is-nav-bar class="ml-auto">
+        <b-collapse id="nav-collapse" class="nav-router" is-nav>
+          <b-navbar-nav>
+            <router-link class="info_prod" to="info_prod">Informazioni prodotto</router-link>
+          </b-navbar-nav>
 
-                <b-nav-item to="login" v-if="isLogged === false">Login</b-nav-item>
-
-                <b-nav-item-dropdown right-alignment v-if="isLogged === true">
-
-                    <template slot="text">
-                    <span style="font-weight: bold;">User</span>
-                    </template>
-
-                    <b-dropdown-item href="#">Il mio profilo</b-dropdown-item>
-                    <b-dropdown-item href="#">Imiei pasti</b-dropdown-item>
-                    <b-dropdown-item href="#">Inserisci nuovo pasto</b-dropdown-item>
-                    <b-dropdown-item @click.prevent="singout">Esci</b-dropdown-item>
-                </b-nav-item-dropdown>
-
-                </b-nav>
-            </b-collapse>
-        </b-navbar>
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item to="login" right v-if="isLogged === false">Login</b-nav-item>
+            <b-nav-item-dropdown right v-if="isLogged === true">
+              <!-- Using 'button-content' slot -->
+              <template v-slot:button-content>
+                <em>User</em>
+              </template>
+                <b-dropdown-item href="#">Il mio profilo</b-dropdown-item>
+                <b-dropdown-item href="#">I miei ultimi pasti</b-dropdown-item>
+                <b-dropdown-item href="#">Inserisci nuovo pasto</b-dropdown-item>
+                <b-dropdown-item @click.prevent="singout">Esci</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
     </div>
 </template>
 
@@ -45,46 +44,7 @@ export default {
 };
 </script>
 
-
-<style>
-  @import './navbar.scss';
-</style>
-
-  <!--
-  <template>
-    <div class="nav-bar">
-      <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-link class="navbar-brand" to="home">Eco-assistant</b-link>
-      <b-nav-toggle target="nav_collapse"></b-nav-toggle>
-      <b-collapse is-nav id="nav_collapse">
-
-        <b-nav is-nav-bar>
-          <b-nav-item to="home">Informazioni prodotto</b-nav-item>
-          <b-nav-item to="about" v-if="isLogged === true">About</b-nav-item>
-        </b-nav>
-
-        <b-nav is-nav-bar class="ml-auto">
-
-          <b-nav-item to="login" v-if="isLogged === false">Login</b-nav-item>
-
-          <b-nav-item-dropdown right-alignment v-if="isLogged === true">
-
-            <template slot="text">
-              <span style="font-weight: bold;">User</span>
-            </template>
-
-            <b-dropdown-item href="#">Il mio profilo</b-dropdown-item>
-            <b-dropdown-item href="#">Imiei pasti</b-dropdown-item>
-            <b-dropdown-item href="#">Inserisci nuovo pasto</b-dropdown-item>
-            <b-dropdown-item @click.prevent="singout">Esci</b-dropdown-item>
-          </b-nav-item-dropdown>
-
-        </b-nav>
-      </b-collapse>
-    </b-navbar>
-</div>
-</template>
-
+<!--
 <script>
 export default {
   name: 'navbar',
@@ -119,4 +79,4 @@ export default {
 }
 </script>
 
-  -->
+-->
