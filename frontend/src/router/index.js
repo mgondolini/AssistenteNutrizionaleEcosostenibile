227d69cf98d0f3b818c4bb61ddff5aa4aa571c31
alessia <alessia.cerami@studio.unibo.it>
2019-11-21 10:59:33 +0100

    login style

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home/Home.vue';
 import About from '../views/About.vue';
-import Login from '../views/Login.vue';
+import Login from '../views/Login/Login.vue';
 
 Vue.use(VueRouter);
 
