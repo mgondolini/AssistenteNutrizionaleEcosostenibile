e727f2721f41cded0597cc6705f0594f867c0f18
alessia <alessia.cerami@studio.unibo.it>
2019-11-25 12:07:41 +0100

    add graphic page

---
+++ frontend/src/views/Graphics/CalculateMeal.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      Hello
+    </p>
+
+    <router-link to='login' class="wrapper-inner-tab-backgrounds-first"
+      v-if='this.$isLogged === false'
+      style="text-decoration:none">
+      <div class="sim-button button1" >
+        <span>Login</span>
+      </div>
+    </router-link>
+  </div>
+</template>
