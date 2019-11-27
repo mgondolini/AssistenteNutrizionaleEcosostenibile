2d44a19ae34ecc67e592d81a63d95422343552b5
mgondolini <monica.gondolini@studio.unibo.it>
2019-11-27 16:22:22 +0100

    implemented add_component

--- backend/src/controllers/mealController.js
+++ backend/src/controllers/mealController.js
@@ -1,13 +1,12 @@
 var mongoose = require('mongoose');
 var Meal = mongoose.model('Meals');
-var MealComponents = mongoose.model('MealComponents');
 
 //TODO: migliorare gestione errori nelle callback
 
 /**
  * Loads all the meals for a given user
  */
-exports.load_meals_list = function(req, res){
+exports.load_meals_list = (req, res) => {
 	console.log("looking for meal...") //DEBUG
 	console.log(req.query); //DEBUG
 
@@ -33,7 +32,7 @@ exports.load_meals_list = function(req, res){
 /**
  * Loads a specific meal of a user
  */
-exports.load_meal = function(req, res){
+exports.load_meal = (req, res) => {
 	console.log("looking for meal to load...") //DEBUG
 
 	var query = {'username': req.query.username};
@@ -63,7 +62,7 @@ exports.load_meal = function(req, res){
 /**
  * Inserts a new meal for a given user
  */
-exports.new_meal = function(req, res) {
+exports.new_meal = (req, res) => {
 	var query = {'username': req.body.username};
 
 	console.log(JSON.stringify(query)); // DEBUG
@@ -87,7 +86,7 @@ exports.new_meal = function(req, res) {
  * @param {*} req 
  * @param {*} res 
  */
-function create_first_meal(req, res) {
+ create_first_meal = (req, res) => {
 	var new_meal = new Meal(req.body);
 	new_meal.save(function(err, meal) { 
 		if (err){
@@ -105,7 +104,7 @@ function create_first_meal(req, res) {
  * @param {*} userMeals 
  * @param {*} res 
  */
-function add_meal(req, userMeals, res) {
+add_meal = (req, userMeals, res) =>{
 
 	//TODO: GESTIONE RES, CONTROLLO SE ESISTE GIà UN update_meal CON LO STESSO NOME (OPPURE usare un id per evitarlo)
 	var meal_to_add = req.body.meals; 
@@ -125,57 +124,22 @@ function add_meal(req, userMeals, res) {
 	});	
 }
 
-
-// cerco il update_meal con il relativo nome, se esiste aggiungo il componente
-
 /**
  * Creates a component for an existing meal
  */
-exports.new_component = function(req, res) {
-	// Oppure passo direttamente tutto il json del meal come query e niente projection
+exports.new_component = async (req, res) => {
 
-	console.log("looking for meal to load...") //DEBUG
-
-	var query = {'username': req.body.username};
-	var projection = {'username': req.body.username, "meals": {$elemMatch: {'meal_name': req.body.mealName}}}
+	var query = {'username': req.body.username, 'meals.meal_name': req.body.mealName};
 	 
-	Meal.find(query, projection, function(err, meal) {
-		if (err){
-			console.log("error while loading meal"); //DEBUG
-			res.send(err);
-		}else{
-			if(meal==null){ //migliorare il controllo
-				res.status(404).send({description: 'meal not found'});
-				console.log("meal not found"); //DEBUG
-			}else{
-				// res.json(meal);
-				console.log("found meal ->"+meal); //DEBUG
-				add_component(req,meal,res);
-			}	
-		}	
-	});
+	await Meal.findOne(query)
+	.exec()
+    .then((doc) => {
+        doc.meals[0].components.push(req.body.components);
+        doc.save((err) => { if (err) throw err; });
+		res.send(doc);
+		//update delle calorie tot ecc
+    })
+    .catch((err) => res.send(err));
 }
 
-function add_component(req, meal, res) {
-	var componenent_to_add = req.body.components; 
-
-	console.log(meal[0].meals[0].components+"aaaaaaa")
-	var update_component = new Meal(meal[0]);
-	console.log("update comp"+update_component+"------------"+JSON.stringify(componenent_to_add))
-	update_component.meals[0].components.push(componenent_to_add);
-
-	// console.log("update_meal"+update_meal+"---"+meal_to_add); //DEBUG
-
-	// non va bene perchè sovrascrive gli altri pasti	
-	// update_component.save(function(err, component){ 
-	// 	if (err){
-	// 		console.log("error while updating new component"); //DEBUG
-	// 		res.send(err);
-	// 	} 
-	// 	console.log("component updated -> "+component.meals[0].component);	//DEBUG
-	// 	res.status(201).json(component);
-	// });	
-
-	Meal.update(meal[0])
-}
 
