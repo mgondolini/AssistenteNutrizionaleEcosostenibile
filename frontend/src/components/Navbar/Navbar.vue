a9d64737fec858c8efb644454058504f8307790a
Alessandro Martignano <alessandro.martignano@yahoo.it>
2019-12-01 15:30:09 +0100

    Example
    -removed auto-generated sample file
    -localized navbar

--- frontend/src/components/Navbar/Navbar.vue
+++ frontend/src/components/Navbar/Navbar.vue
@@ -9,7 +9,7 @@
 
         <b-collapse id="nav-collapse" class="nav-router" is-nav>
           <b-navbar-nav>
-            <router-link class="info_prod" to="info_prod">Informazioni prodotto</router-link>
+            <router-link class="info_prod" to="info_prod">{{ $t('prod_info') }}</router-link>
           </b-navbar-nav>
 
           <!-- Right aligned nav items -->
@@ -48,6 +48,17 @@ export default {
   },
 };
 </script>
+
+<i18n>
+{
+  "en": {
+    "prod_info": "Product informations"
+  },
+  "it": {
+    "prod_info": "Informazioni prodotto"
+  }
+}
+</i18n>
 
 <!--
 <script>
