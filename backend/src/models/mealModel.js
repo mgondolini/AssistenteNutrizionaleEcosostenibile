9aac5a273e807b95ad51f8692f199124f86b9f0f
Andrea Giordano <andrea.giordano5@studio.unibo.it>
2019-12-01 16:00:24 +0100

    Linted

--- backend/src/models/mealModel.js
+++ backend/src/models/mealModel.js
@@ -1,30 +1,31 @@
 const mongoose = require('mongoose');
+
 const { Schema } = mongoose;
 
 const MealComponentSchema = new Schema({
-    barcode: String,
-    product_name: String,
-    quantity: Number
-}, { _id : false });
+  barcode: String,
+  product_name: String,
+  quantity: Number,
+}, { _id: false });
 
 const MealSchema = new Schema({
-    meal_name: String,
-    components: [MealComponentSchema],
-    energy_tot: Number,
-    carbohydrates_tot: Number,
-    sugars_tot: Number,
-    fat_tot: Number,
-    saturated_fat_tot: Number,
-    proteins_tot: Number,
-    fiber_tot: Number,
-    salt_tot: Number,
-    sodium_tot: Number,
-    alcohol_tot: Number,
-    calcium_tot: Number,
-    carbon_footprint_tot: Number,
-    water_footprint_tot: Number, 
-    timestamp: String
-}, { _id : false });
+  meal_name: String,
+  components: [MealComponentSchema],
+  energy_tot: Number,
+  carbohydrates_tot: Number,
+  sugars_tot: Number,
+  fat_tot: Number,
+  saturated_fat_tot: Number,
+  proteins_tot: Number,
+  fiber_tot: Number,
+  salt_tot: Number,
+  sodium_tot: Number,
+  alcohol_tot: Number,
+  calcium_tot: Number,
+  carbon_footprint_tot: Number,
+  water_footprint_tot: Number,
+  timestamp: String,
+}, { _id: false });
 
 const UserMealSchema = new Schema({
   username: String,
