d153e748b031bf34334830be2b91c7093c46d67f
Monica <monica.gondolini@studio.unibo.it>
2019-12-05 16:42:24 +0100

    Created mealControllerUtils

--- backend/src/controllers/mealController.js
+++ backend/src/controllers/mealController.js
@@ -6,119 +6,6 @@ const SingleMeal = mongoose.model('SingleMeal');
 const Products = mongoose.model('Product');
 
 
-const computeValuePerPortion = (value, quantity) => ((value / 100) * quantity);
-
-const valuePerPortion = (value, quantity) => {
-  let valuePerQuantity;
-
-  if (value != null) valuePerQuantity = computeValuePerPortion(value, quantity);
-  else valuePerQuantity = 0;
-
-  return valuePerQuantity;
-};
-
-
-const computeMealValues = async (barcode, quantity, res) => {
-  let productName;
-  let imageUrl;
-  let energyTot;
-  let carbsTot;
-  let sugarsTot;
-  let fatTot;
-  let saturatedFatTot;
-  let proteinsTot;
-  let fiberTot;
-  let saltTot;
-  let sodiumTot;
-  let alcoholTot;
-  let calciumTot;
-  let carbonFootprintTot;
-  let waterFootprintTot;
-
-  const query = { code: barcode };
-
-  await Products.findOne(query)
-    .exec()
-    .then((product) => {
-      productName = product.product_name;
-      imageUrl = product.image_url;
-      energyTot = valuePerPortion(product.energy_100g, quantity);
-      carbsTot = valuePerPortion(product.carbohydrates_100g, quantity);
-      sugarsTot = valuePerPortion(product.sugars_100g, quantity);
-      fatTot = valuePerPortion(product.fat_100g, quantity);
-      saturatedFatTot = valuePerPortion(product.saturated_fat_100g, quantity);
-      proteinsTot = valuePerPortion(product.proteins_100g, quantity);
-      fiberTot = valuePerPortion(product.fiber_100g, quantity);
-      saltTot = valuePerPortion(product.salt_100g, quantity);
-      sodiumTot = valuePerPortion(product.sodium_100g, quantity);
-      alcoholTot = valuePerPortion(product.alcohol_100g, quantity);
-      calciumTot = valuePerPortion(product.calcium_100g, quantity);
-      carbonFootprintTot = valuePerPortion(product.carbon_footprint_100g, quantity);
-      waterFootprintTot = valuePerPortion(product.water_footprint_100g, quantity);
-    })
-    .catch((err) => res.send(err));
-
-  const values = {
-    product_name: productName,
-    image_url: imageUrl,
-    energy_tot: energyTot,
-    carbohidrates_tot: carbsTot,
-    sugars_tot: sugarsTot,
-    fat_tot: fatTot,
-    saturated_fat_tot: saturatedFatTot,
-    proteins_tot: proteinsTot,
-    salt_tot: saltTot,
-    sodium_tot: sodiumTot,
-    calcium_tot: calciumTot,
-    alcohol_tot: alcoholTot,
-    fiber_tot: fiberTot,
-    carbon_footprint_tot: carbonFootprintTot,
-    water_footprint_tot: waterFootprintTot,
-  };
-  console.log(`VALUES -> ${JSON.stringify(values)}`); // DEBUG
-
-  return values;
-};
-
-
-const updateMealValues = async (components, mealName, userMeals, res) => {
-  const { barcode } = components;
-  const { quantity } = components;
-
-  computeMealValues(barcode, quantity, res)
-    .then((values) => {
-      userMeals.meals.forEach((meal) => {
-        if (meal.meal_name === mealName) {
-          // Meal schema field update
-          meal.energy_tot += values.energy_tot;
-          meal.carbohidrates_tot += values.carbohidrates_tot;
-          meal.sugars_tot += values.sugars_tot;
-          meal.fat_tot += values.fat_tot;
-          meal.saturated_fat_tot += values.saturated_fat_tot;
-          meal.proteins_tot += values.proteins_tot;
-          meal.salt_tot += values.salt_tot;
-          meal.sodium_tot += values.sodium_tot;
-          meal.calcium_tot += values.calcium_tot;
-          meal.alcohol_tot += values.alcohol_tot;
-          meal.fiber_tot += values.fiber_tot;
-          meal.carbon_footprint_tot += values.carbon_footprint_tot;
-          meal.water_footprint_tot += values.water_footprint_tot;
-
-          // Components schema field update
-          components.product_name = values.product_name;
-          components.image_url = values.image_url;
-
-          console.log(`components: ${components}`); // DEBUG
-          meal.components.push(components);
-        }
-      });
-      userMeals.save()
-        .then((meals) => res.status(201).json(meals))
-        .catch((err) => res.send(err));
-    });
-};
-
-
 /**
  * Loads all the meals for a given user
  */
@@ -167,6 +54,7 @@ exports.load_meal = async (req, res) => {
     })
     .catch((err) => res.send(err));
 };
+
 
 const initMealValues = (mealName) => {
   const mealToAdd = new SingleMeal();
@@ -268,6 +156,116 @@ exports.new_meal = async (req, res) => {
     .catch((err) => res.send(err));
 };
 
+
+const computeValuePerPortion = (value, quantity) => ((value / 100) * quantity);
+
+const valuePerPortion = (value, quantity) => {
+  let valuePerQuantity;
+
+  if (value != null) valuePerQuantity = computeValuePerPortion(value, quantity);
+  else valuePerQuantity = 0;
+
+  return valuePerQuantity;
+};
+
+const computeMealValues = async (barcode, quantity, res) => {
+  let productName;
+  let imageUrl;
+  let energyTot;
+  let carbsTot;
+  let sugarsTot;
+  let fatTot;
+  let saturatedFatTot;
+  let proteinsTot;
+  let fiberTot;
+  let saltTot;
+  let sodiumTot;
+  let alcoholTot;
+  let calciumTot;
+  let carbonFootprintTot;
+  let waterFootprintTot;
+
+  const query = { code: barcode };
+
+  await Products.findOne(query)
+    .exec()
+    .then((product) => {
+      productName = product.product_name;
+      imageUrl = product.image_url;
+      energyTot = valuePerPortion(product.energy_100g, quantity);
+      carbsTot = valuePerPortion(product.carbohydrates_100g, quantity);
+      sugarsTot = valuePerPortion(product.sugars_100g, quantity);
+      fatTot = valuePerPortion(product.fat_100g, quantity);
+      saturatedFatTot = valuePerPortion(product.saturated_fat_100g, quantity);
+      proteinsTot = valuePerPortion(product.proteins_100g, quantity);
+      fiberTot = valuePerPortion(product.fiber_100g, quantity);
+      saltTot = valuePerPortion(product.salt_100g, quantity);
+      sodiumTot = valuePerPortion(product.sodium_100g, quantity);
+      alcoholTot = valuePerPortion(product.alcohol_100g, quantity);
+      calciumTot = valuePerPortion(product.calcium_100g, quantity);
+      carbonFootprintTot = valuePerPortion(product.carbon_footprint_100g, quantity);
+      waterFootprintTot = valuePerPortion(product.water_footprint_100g, quantity);
+    })
+    .catch((err) => res.send(err));
+
+  const values = {
+    product_name: productName,
+    image_url: imageUrl,
+    energy_tot: energyTot,
+    carbohidrates_tot: carbsTot,
+    sugars_tot: sugarsTot,
+    fat_tot: fatTot,
+    saturated_fat_tot: saturatedFatTot,
+    proteins_tot: proteinsTot,
+    salt_tot: saltTot,
+    sodium_tot: sodiumTot,
+    calcium_tot: calciumTot,
+    alcohol_tot: alcoholTot,
+    fiber_tot: fiberTot,
+    carbon_footprint_tot: carbonFootprintTot,
+    water_footprint_tot: waterFootprintTot,
+  };
+  console.log(`VALUES -> ${JSON.stringify(values)}`); // DEBUG
+
+  return values;
+};
+
+const updateMealValues = async (components, mealName, userMeals, res) => {
+  const { barcode } = components;
+  const { quantity } = components;
+
+  computeMealValues(barcode, quantity, res)
+    .then((values) => {
+      userMeals.meals.forEach((meal) => {
+        if (meal.meal_name === mealName) {
+          // Meal schema field update
+          meal.energy_tot += values.energy_tot;
+          meal.carbohidrates_tot += values.carbohidrates_tot;
+          meal.sugars_tot += values.sugars_tot;
+          meal.fat_tot += values.fat_tot;
+          meal.saturated_fat_tot += values.saturated_fat_tot;
+          meal.proteins_tot += values.proteins_tot;
+          meal.salt_tot += values.salt_tot;
+          meal.sodium_tot += values.sodium_tot;
+          meal.calcium_tot += values.calcium_tot;
+          meal.alcohol_tot += values.alcohol_tot;
+          meal.fiber_tot += values.fiber_tot;
+          meal.carbon_footprint_tot += values.carbon_footprint_tot;
+          meal.water_footprint_tot += values.water_footprint_tot;
+
+          // Components schema field update
+          components.product_name = values.product_name;
+          components.image_url = values.image_url;
+
+          console.log(`components: ${components}`); // DEBUG
+          meal.components.push(components);
+        }
+      });
+      userMeals.save()
+        .then((meals) => res.status(201).json(meals))
+        .catch((err) => res.send(err));
+    });
+};
 
 /**
  * Creates a component for an existing meal
