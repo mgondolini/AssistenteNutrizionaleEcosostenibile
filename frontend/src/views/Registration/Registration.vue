8cef83f27de27100c98b3a73ef3dba1c96a6ea92
Andrea Giordano <andrea.giordano5@studio.unibo.it>
2019-12-21 11:11:11 +0100

    tmp

--- frontend/src/views/Registration/Registration.vue
+++ frontend/src/views/Registration/Registration.vue
@@ -3,6 +3,18 @@
     <b-card title="Registrazione">
       <b-form @submit="onSubmit" class="formIns">
         <b-form-group
+          id="input-group-username"
+          label="Username:"
+          label-for="input-username"
+        >
+          <b-form-input
+            id="input-username"
+            v-model="form.username.value"
+            required
+            placeholder="Inserisci username"
+          ></b-form-input>
+        </b-form-group>
+        <b-form-group
           id="input-group-email"
           label="Email:"
           label-for="input-email"
@@ -45,48 +57,37 @@
           ></b-form-input>
         </b-form-group>
         <b-form-group
-          id="input-group-username"
-          label="Username:"
-          label-for="input-username"
-        >
-          <b-form-input
-            id="input-username"
-            v-model="form.username"
-            required
-            placeholder="Inserisci username"
-          ></b-form-input>
-        </b-form-group>
-        <b-form-group
           id="input-group-personal"
           label="Dati personali:"
+          label-for="input-name"
         >
           <b-form-input
             id="input-name"
-            v-model="form.name"
+            v-model="form.name.value"
             required
             placeholder="Inserisci nome"
           ></b-form-input>
           <b-form-input
             id="input-surname"
-            v-model="form.surname"
+            v-model="form.surname.value"
             required
             placeholder="Inserisci cognome"
           ></b-form-input>
           <b-form-input
             id="input-height"
-            v-model="form.height"
+            v-model="form.height.value"
             required
             placeholder="Inserisci altezza"
           ></b-form-input>
           <b-form-input
             id="input-weight"
-            v-model="form.weight"
+            v-model="form.weight.value"
             required
             placeholder="Inserisci peso"
           ></b-form-input>
           <b-form-input
             id="input-allergens"
-            v-model="form.allergens"
+            v-model="form.allergens.value"
             required
             placeholder="Inserisci allergeni"
             aria-describedby="allergens-help-block"
@@ -94,12 +95,19 @@
           <b-form-text id="allergens-help-block">
             Gli allergeni devono essere separati da spazio.
           </b-form-text>
-          <b-form-input
+          <!--<b-form-input
             id="input-sex"
             v-model="form.sex"
             required
             placeholder="Inserisci sesso"
-          ></b-form-input>
+          ></b-form-input>-->
+          <b-form-select v-model="form.sex">
+            <option disabled value=""> {{ $t(`${form.sex.key}`) }}</option>
+            <option v-for="opt in form.sex.ar"
+            v-bind:key="opt" v-bind:value="opt">
+              {{ opt.toUpperCase() }}
+            </option>
+          </b-form-select>
         </b-form-group>
         <div class="text-center">
           <b-button type="submit" variant="primary">Registrami</b-button>
@@ -118,15 +126,43 @@ export default {
         email: '',
         password: '',
         repassword: '',
-        username: '',
-        name: '',
-        surname: '',
-        height: '',
-        weight: '',
+        username: {
+          key: 'username',
+          value: '',
+          placeholder: 'Enter your username',
+        },
+        name: {
+          key: 'name',
+          value: '',
+          placeholder: 'Enter your name',
+        },
+        surname: {
+          key: 'surname',
+          value: '',
+          placeholder: 'Enter your surname',
+        },
+        height: {
+          key: 'height',
+          value: '',
+          placeholder: 'Enter your height',
+        },
+        weight: {
+          key: 'weight',
+          value: '',
+          placeholder: 'Enter your weight',
+        },
         dateOfBirth: '',
-        sex: '',
+        sex: {
+          key: 'gender',
+          value: '',
+          ar: ['m', 'f'],
+        },
         img: '',
-        allergens: '',
+        allergens: {
+          key: 'allergens',
+          value: '',
+          placeholder: 'Enter your allergens',
+        },
       },
     };
   },
