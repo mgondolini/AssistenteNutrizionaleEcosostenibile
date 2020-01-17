ef447d791f983e14bbe759ad0ad0834b022b4750
alessia <alessia.cerami@studio.unibo.it>
2020-01-17 16:07:15 +0100

    Meal graph fix

--- frontend/src/views/CalculateMeal/CalculateMeal.vue
+++ frontend/src/views/CalculateMeal/CalculateMeal.vue
@@ -6,7 +6,7 @@
         v-on:click='changeGraphGlobal'>
         Global
       </b-button>
-      <b-button id="button" class="sim-button button1 buttonCalculate"
+      <b-button v-show="this.buttonToShow" id="button" class="sim-button button1 buttonCalculate"
         v-on:click='changeGraphComponent'>
         Component
       </b-button>
@@ -21,6 +21,14 @@
       <apexchart type=bubble height=350 width=600 :options="chartBubbleWaterOptions"
       :series="series2" />
     </div>
+    <b-modal id="modal-error" title="Error"
+      hide-footer v-model="modalErrorShow">
+      <div class="d-block text-center">
+        <img src="https://img.icons8.com/color/48/000000/restriction-shield.png">
+        {{ this.errorMsgModal }}
+      </div>
+      <b-button class="mt-3" block @click="hideModal">{{ $t('closeBtn')}}</b-button>
+    </b-modal>
   </div>
 </template>
 
@@ -36,6 +44,10 @@ export default {
       composition: { al: [], av: [] },
       arr: [],
       waterFootprint: [],
+      errorMsgModal: '',
+      modalErrorShow: false,
+      toLogin: false,
+      buttonToShow: false,
       series: [{
         name: '',
         data: [],
@@ -109,11 +121,11 @@ export default {
             offsetX: 0,
             offsetY: 0,
           },
-          title: { text: 'CO2 per grammo' },
+          title: { text: this.$i18n.t('CO2grams') },
         },
         yaxis: {
           max: 600,
-          title: { text: 'grammi consumati' },
+          title: { text: this.$i18n.t('gCons') },
           labels: {
             showDuplicates: false,
           },
@@ -164,14 +176,14 @@ export default {
             offsetX: 0,
             offsetY: 0,
           },
-          title: { text: '{{$t(\'watergrams\')}}' },
+          title: { text: this.$i18n.t('watergrams') },
         },
         yaxis: {
           max: 600,
           labels: {
             showDuplicates: false,
           },
-          title: { text: 'grammi consumati' },
+          title: { text: this.$i18n.t('gCons') },
         },
         responsive: [{
           breakpoint: 480,
@@ -188,12 +200,10 @@ export default {
     apexchart: VueApexCharts,
   },
   mounted() {
-    // prendere username da sessione
-    const username = 'mrossi';
-    const { mealName } = this.$route.query;
-    const { date } = this.$route.query;
-    const params = { username, mealName, date };
-    this.$store.state.http.get(`api/${params.username}/meals/${params.mealName}`, { params })
+    const params = { mealName: this.$route.query.mealName, date: this.$route.query.date };
+    console.log(`Calculate_meal_comp mounted ${params.mealName}`);
+    this.modalErrorShow = false;
+    this.$store.state.http.get('api/meal', { params })
       .then((response) => {
         response.data.meals[0].components.forEach((elem) => {
           this.composition.al.push(elem.product_name.concat(' - ').concat(elem.quantity).concat(' g'));
@@ -221,13 +231,34 @@ export default {
 
         Object.values(response.data.meals[0]).forEach((v, i) => {
           if (i > 2 && i < 13) {
-            if (v > 0.0001) this.componentsMeal.av.push(v);
+            if (v > 0.0001) {
+              this.componentsMeal.av.push(v);
+              this.buttonToShow = true;
+            } else {
+              this.buttonToShow = false;
+            }
           }
         });
         this.calculate();
-      }).catch(error => (console.log(error)));
+      }).catch(error => this.checkError(error.description));
   },
   methods: {
+    checkError(error, status) {
+      if (status === 401) {
+        this.errorMsgModal = this.$i18n.t('unauthorized');
+        this.modalErrorShow = true;
+        this.toLogin = true;
+      } else {
+        this.toLogin = false;
+        this.errorMsgModal = this.$i18n.t('error');
+        this.modalErrorShow = true;
+      }
+    },
+    hideModal() {
+      this.$bvModal.hide('modal-error');
+      if (this.toLogin) this.$router.push('/login');
+      else this.$router.push('/meals');
+    },
     changeGraphGlobal() {
       this.chart.al = this.composition.al;
       this.chart.av = this.composition.av;
@@ -260,13 +291,19 @@ export default {
     "intro": "Composition of your meal",
     "CO2grams": "CO2 per grams",
     "gCons":"Consumed grams",
-    "watergrams": "Water per grams"
+    "watergrams": "Water per grams",
+    "error": "Errors occurred. Try again!",
+    "unauthorized": "Unauthorized access",
+    "closeBtn": "Close me!"
   },
   "it": {
     "intro": "Coposizione del tuo pasto",
     "CO2grams": "CO2 per grammi",
     "gCons":"Grammi consumati",
-    "watergrams": "Acqua per grammi"
+    "watergrams": "Acqua per grammi",
+    "error": "Sono avvenuti degli errori. Riprova dopo!",
+    "unauthorized": "Accesso non autorizzato",
+    "closeBtn": "Chiudi!"
   }
 }
 </i18n>
