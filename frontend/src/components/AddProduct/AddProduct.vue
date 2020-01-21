5071b4c042054651336f045c29e02e36136c7dd3
Alessandro Martignano <alessandro.martignano@yahoo.it>
2020-01-21 20:03:20 +0100

    Created AddProduct component

---
+++ frontend/src/components/AddProduct/AddProduct.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'AddProduct',
+  data() {
+    return {
+    };
+  },
+  methods: {
+  },
+  mounted() {
+    console.log('AddProduct MOUNTED!');
+  },
+};
+</script>
+
+<i18n>
+{
+  "en": {
+
+  },
+  "it": {
+
+  }
+}
+</i18n>
