3204f5e86d5a3dc65c04f3c77e2dbe0394496722
Alessandro Martignano <alessandro.martignano@yahoo.it>
2020-01-22 03:43:40 +0100

    Style fix

--- frontend/src/components/AddProduct/AddProduct.vue
+++ frontend/src/components/AddProduct/AddProduct.vue
@@ -1,11 +1,12 @@
 <template>
   <b-modal
     id="modal-addProduct"
-    title="Error"
+    title="Lookup modes"
+    centered
     hide-footer
     @close="inputMode = 'SELECT'"
     >
-    <div v-if="inputMode === 'SELECT'" class="buttonContainer">
+    <div v-if="inputMode === 'SELECT'" class="buttonContainerVertical">
         <b-button v-on:click="inputMode = 'MANUAL'">Manual insert</b-button>
         <b-button v-on:click="inputMode = 'STREAM'">Scan barcode</b-button>
         <b-button v-on:click="uploadFile()">Upload barcode</b-button>
@@ -19,10 +20,12 @@
           v-model="ean"
           value=""
         >
-        <b-form-select v-model="ean" :options="eanOptions"></b-form-select>
       </div>
-      <b-button v-on:click="submitEan()">Lookup</b-button>
-      <b-button v-on:click="inputMode = 'SELECT'">Back</b-button>
+      <b-form-select v-model="ean" :options="eanOptions"></b-form-select>
+      <div>
+        <b-button v-on:click="submitEan()">Lookup</b-button>
+        <b-button v-on:click="inputMode = 'SELECT'">Back</b-button>
+      </div>
     </div>
     <div v-else-if="inputMode === 'STREAM'" id="videoStream" class="buttonContainer">
       <v-quagga
@@ -55,6 +58,17 @@ export default {
       },
       aspectRatio: { min: 1, max: 2 },
       detecteds: [],
+
+      // ean dropdown selector facility
+      eanOptions: [
+        { value: '737628064502', text: 'Noodles' },
+        { value: '20969578', text: 'Sbrisolona' },
+        { value: '8001300240785', text: 'Tonno' },
+        { value: '5411188110835', text: 'Latte' },
+        { value: '4104420208629', text: 'Spaghetti' },
+        { value: '3560070240258', text: 'Chips' },
+        { value: '8001300500773', text: 'Lievito' },
+      ],
     };
   },
   methods: {
@@ -97,3 +111,7 @@ export default {
   }
 }
 </i18n>
+
+<style lang="sass">
+  @import './AddProduct.sass';
+</style>
