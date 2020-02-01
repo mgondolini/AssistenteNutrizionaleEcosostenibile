e5b99c2166171bd24a9fbc2c149a0cbde2d8ccaa
Monica <monica.gondolini@studio.unibo.it>
2020-02-01 14:34:15 +0100

    Added BubblePieChart.vue

---
+++ frontend/src/views/CalculateMeal/BubblePieChart.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="BubblePieChart">
+    <h3>BubblePieChart</h3>
+    <div class="chart-box">
+
+    <apexchart class="co2" type=b
+ble height=400 :options="chartBubbleCO2Options"
+        :series="se
+es">
+
+
+</apexchart
+
+    </div
+
+  </div>
+
+template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
