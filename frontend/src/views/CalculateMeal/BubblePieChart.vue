5860918e64fa4ad6cf50c9e762d59c3639c9940f
Monica <monica.gondolini@studio.unibo.it>
2020-02-07 15:33:06 +0100

    Method to fill data

--- frontend/src/views/CalculateMeal/BubblePieChart.vue
+++ frontend/src/views/CalculateMeal/BubblePieChart.vue
@@ -36,6 +36,26 @@ export default {
   },
   data() {
     return {
+      // Nutritional values
+      ingredients: [],
+      carbohydrates: [],
+      sugars: [],
+      fats: [],
+      saturatedFats: [],
+      proteins: [],
+      salt: [],
+      sodium: [],
+      calcium: [],
+      alcohol: [],
+      fibers: [],
+
+      // Bubble dimension
+      redius: [],
+
+      // Labels
+      bubbleLabels: [],
+
+      // Chart
       chartData: {
         type: 'bubble-pie',
         scaleX: {
@@ -58,26 +78,21 @@ export default {
             [6, 12, 101],
             [8, 12, 101],
           ],
+          dataBubble: null,
+          tooltip: {
+            text: '%data-pie: %data-v',
+            'font-color': 'blue',
+            'font-family': 'Georgia, serif',
+            'background-color': 'white',
+            'border-color': 'pink',
+            'border-width': 2,
+            'line-style': 'dashed',
+          },
+          scaling: 'area',
+          sizeFactor: 4,
         },
       },
-      series: [
-        { 'data-v': [15, 37, 7, 3, 14] },
-        { 'data-v': [13, 34, 21, 7, 8] },
-        { 'data-v': [6, 30, 31, 5, 8] },
-        { 'data-v': [5, 29, null, 3, 13] },
-        { 'data-v': [3, 25, 19, 3, null] },
-      ],
-
-      //       { 'Tonno all olio di oliva': [811, 811, null] },
-      // { 'Mandel Original': [102, 102, 3] },
-
-      ingredients: [],
-
-      // { 'data-v': [15, 37, 7, 3, 14] },
-      // { 'data-v': [13, 34, 21, 7, 8] },
-      // { 'data-v': [6, 30, 31, 5, 8] },
-      // { 'data-v': [5, 29, null, 3, 13] },
-      // { 'data-v': [3, 25, 19, 3, null] },
+      series: [],
     };
   },
   computed: {
@@ -102,12 +117,11 @@ export default {
 
           // Will be triggered when ALL the promises ar fullfilled or rejected
           Promise.all(promises)
-            // "responses" is an iterable array of all the promises' responses
             .then((responses) => {
               responses.forEach((res) => {
                 this.ingredients.push(res.data);
               });
-              // this.createGraphData();
+              this.createGraphData();
             })
             .catch(err => console.log(err));
         })
@@ -117,24 +131,64 @@ export default {
         });
     },
     loadProductValues(barcode, quantity) {
+      const { http } = this.$store.state;
       const params = {
         barcode,
         quantity,
       };
-      return this.$store.state.http.get(`api/product/${params.barcode}/${params.quantity}`, { params });
+
+      return http.get(`api/product/${params.barcode}/${params.quantity}`, { params });
     },
     createGraphData() {
+      console.log(this.ingredients);
+
       this.ingredients.forEach((i) => {
-        this.series.push({ [i.product_name]: this.getIngredientComposition(i) });
+        this.carbohydrates.push(Number((i.carbohydrates_tot).toFixed(2)));
+        this.sugars.push(Number((i.sugars_tot).toFixed(2)));
+        this.fats.push(Number((i.fat_tot).toFixed(2)));
+        this.saturatedFats.push(Number((i.saturated_fat_tot).toFixed(2)));
+        this.proteins.push(Number((i.proteins_tot).toFixed(2)));
+        this.salt.push(Number((i.salt_tot).toFixed(2)));
+        this.sodium.push(Number((i.sodium_tot).toFixed(2)));
+        this.calcium.push(Number((i.calcium_tot).toFixed(2)));
+        this.alcohol.push(Number((i.alcohol_tot).toFixed(2)));
+        this.fibers.push(Number((i.fiber_tot).toFixed(2)));
+
+        this.bubbleLabels.push(i.product_name);
       });
-      console.log(`series${JSON.stringify(this.series)}`);
+
+      this.series = this.populateSeries();
+      this.chartData.plot.dataBubble = this.bubbleLabels;
+      console.log(this.chartData.plot.dataBubble);
+
+      console.log('series');
       console.log(this.series);
     },
-    getIngredientComposition(i) {
-      return [i.energy_kj_tot, i.energy_kj_tot, i.carbohydrates_tot, i.sugars_tot,
-        i.fat_tot, i.saturated_fat_tot, i.proteins_tot, i.salt_tot,
-        i.sodium_tot, i.calcium_tot, i.alcohol_tot, i.fiber_tot];
+    populateSeries() {
+      return [
+        this.createObject('Carbohydrates', this.carbohydrates),
+        this.createObject('Sugars', this.sugars),
+        this.createObject('Fats', this.fats),
+        this.createObject('SaturatedFats', this.saturatedFats),
+        this.createObject('Proteins', this.proteins),
+        this.createObject('Salt', this.salt),
+        this.createObject('Sodium', this.sodium),
+        this.createObject('Calcium', this.calcium),
+        this.createObject('Alcohol', this.alcohol),
+        this.createObject('Fibers', this.fibers),
+      ];
     },
+    createObject(name, values) {
+      return {
+        'data-v': values,
+        'data-pie': name,
+      };
+    },
+    // getIngredientComposition(i) {
+    //   return [i.energy_kj_tot, i.energy_kj_tot, i.carbohydrates_tot, i.sugars_tot,
+    //     i.fat_tot, i.saturated_fat_tot, i.proteins_tot, i.salt_tot,
+    //     i.sodium_tot, i.calcium_tot, i.alcohol_tot, i.fiber_tot];
+    // },
     createGraph() {
       console.log('graphData');
       console.log(this.graphData);
