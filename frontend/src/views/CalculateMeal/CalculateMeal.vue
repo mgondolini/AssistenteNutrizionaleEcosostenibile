49b23c50c5aa74cdab827d4da184bbf603ab7502
Monica <monica.gondolini@studio.unibo.it>
2020-02-16 11:32:48 +0100

    Added bubble pie to calculateMeal

--- frontend/src/views/CalculateMeal/CalculateMeal.vue
+++ frontend/src/views/CalculateMeal/CalculateMeal.vue
@@ -34,6 +34,13 @@
             </div>
           </div>
         </b-tab>
+        <b-tab class="tab-content-info" :title="$t('ingredients')">
+          <div class="chart-box">
+            <!-- <div id="bubble-chart"> -->
+              <zingchart ref="bubblepie" :data="bubbleChartData" :series="bubbleSeries"></zingchart>
+            <!-- </div> -->
+          </div>
+        </b-tab>
       </b-tabs>
       <b-modal id="modal-error" :title="$i18n.t('errorModalTitle')"
         hide-footer v-model="modalErrorShow">
@@ -49,6 +56,14 @@
 
 <script>
 import VueApexCharts from 'vue-apexcharts';
+import zingchartVue from 'zingchart-vue';
+
+// TODO: trovare un modo di importarlo migliore
+// eslint-disable-next-line import/extensions
+import '../../../node_modules/zingchart/modules/zingchart-bubblepie.min.js';
+
+// Vue.component('zingchart', zingchartVue);
+
 
 export default {
   name: 'CalculateMeal',
@@ -70,6 +85,32 @@ export default {
         name: '',
         data: [],
       }],
+
+      // Nutritional values
+      ingredients: [],
+      carbohydrates: [],
+      sugars: [],
+      fats: [],
+      saturatedFats: [],
+      proteins: [],
+      salt: [],
+      sodium: [],
+      calcium: [],
+      alcohol: [],
+      fibers: [],
+
+      // Coordinates and radius of the bubbles
+      values: [],
+
+      // Bubble dimension
+      radius: [],
+
+      // Labels
+      bubbleLabels: [],
+
+      // Series
+      bubbleSeries: [],
+
     };
   },
   computed: {
@@ -193,13 +234,105 @@ export default {
         },
       };
     },
+    bubbleChartData() {
+      return {
+        type: 'bubble-pie',
+        height: '100%',
+        width: '80%',
+        x: '0%',
+        y: '0%',
+        scaleX: {
+          minValue: 0,
+          maxValue: 50,
+          step: 5,
+          lineColor: 'none',
+          guide: {
+            lineColor: 'none',
+          },
+        },
+        scaleY: {
+          minValue: 0,
+          maxValue: 50,
+          step: 1,
+          lineColor: 'none',
+          guide: {
+            lineColor: 'none',
+          },
+        },
+        legend: {
+          item: {
+            text: '%data-pie',
+            fontSize: 12,
+          },
+          width: '10%',
+          align: 'right',
+          verticalAlign: 'middle',
+        },
+        plot: {
+          values: this.values,
+          dataBubble: this.bubbleLabels,
+          tooltip: {
+            text: '%data-pie: %data-v',
+            fontColor: 'blue',
+            fontFamily: 'Georgia, serif',
+            backgroundColor: 'white',
+            borderColor: 'blue',
+            borderWidth: 2,
+          },
+          minSize: 30,
+          maxSize: 50,
+          scaling: 'radius',
+          sizeFactor: 5,
+        },
+        mediaRules: [
+          {
+            maxWidth: 500,
+            width: '100%',
+            legend: {
+              align: 'right',
+              verticalAlign: 'top',
+              layout: '5x2',
+              width: '60%',
+              item: {
+                fontSize: 10,
+              },
+            },
+            plot: {
+              values: this.values,
+              dataBubble: this.bubbleLabels,
+              tooltip: {
+                text: '%data-pie: %data-v',
+                fontColor: 'blue',
+                fontFamily: 'Georgia, serif',
+                backgroundColor: 'white',
+                borderColor: 'blue',
+                borderWidth: 2,
+                lineStyle: 'normal',
+              },
+              minSize: 10,
+              maxSize: 20,
+              scaling: 'radius',
+              sizeFactor: 5,
+            },
+          },
+          {
+            maxWidth: 300,
+            label: {
+              visible: false,
+            },
+          },
+        ],
+      };
+    },
   },
   components: {
     apexchart: VueApexCharts,
+    zingchart: zingchartVue,
   },
   mounted() {
     const params = { mealName: this.$route.query.mealName, date: this.$route.query.date };
     this.toLogin = false;
+    const promises = [];
     this.$store.state.http.get('api/meal', { params })
       .then((response) => {
         response.data.meals[0].components.forEach((elem) => {
@@ -231,10 +364,125 @@ export default {
         });
 
         this.calculate();
+
+        [this.meal] = response.data.meals;
+        this.meal.components.forEach((c) => {
+          const promise = this.loadProductValues(c.barcode, c.quantity);
+          this.radius.push(c.quantity);
+          promises.push(promise);
+        });
+
+        Promise.all(promises)
+          .then((responses) => {
+            responses.forEach((res) => {
+              this.ingredients.push(res.data);
+            });
+            this.createGraphData();
+          })
+          .catch(err => console.log(err));
       }).catch(error => this.checkError(error.response.data.description,
         error.response.status));
   },
   methods: {
+    loadProductValues(barcode, quantity) {
+      const { http } = this.$store.state;
+      const params = {
+        barcode,
+        quantity,
+      };
+
+      return http.get(`api/product/${params.barcode}/${params.quantity}`, { params });
+    },
+    createGraphData() {
+      let x;
+      let y;
+
+      let j = 0;
+
+      console.log('Radius');
+      console.log(this.radius);
+
+      // console.log('OLD VALUES');
+      // console.log(this.bubbleChartData.plot.values);
+      // console.log('OLD SERIES');
+      console.log(this.series);
+
+
+      this.values = [];
+      this.dataBubble = [];
+      this.bubbleSeries = null;
+
+      this.ingredients.forEach((i) => {
+        this.carbohydrates.push(Number((i.carbohydrates_tot).toFixed(2)));
+        this.sugars.push(Number((i.sugars_tot).toFixed(2)));
+        this.fats.push(Number((i.fat_tot).toFixed(2)));
+        this.saturatedFats.push(Number((i.saturated_fat_tot).toFixed(2)));
+        this.proteins.push(Number((i.proteins_tot).toFixed(2)));
+        this.salt.push(Number((i.salt_tot).toFixed(2)));
+        this.sodium.push(Number((i.sodium_tot).toFixed(2)));
+        this.calcium.push(Number((i.calcium_tot).toFixed(2)));
+        this.alcohol.push(Number((i.alcohol_tot).toFixed(2)));
+        this.fibers.push(Number((i.fiber_tot).toFixed(2)));
+
+        x = i.carbon_footprint_tot;
+        y = i.water_footprint_tot;
+
+        console.log(`coords (${x}, ${y})`);
+
+        this.values.push([x, y, this.radius[j]]);
+        this.bubbleLabels.push(i.product_name);
+
+        j += 1;
+      });
+      console.log('bubble labels');
+      console.log(this.bubbleLabels);
+      // console.log('NEW VALUES');
+      // console.log(this.BubbleChartData.plot.values);
+      console.log('NEW SERIES');
+      this.bubbleSeries = this.populateSeries();
+      console.log(this.bubbleSeries);
+
+      this.ready = true;
+    },
+    populateSeries() {
+      return [
+        this.createObject('carbohydrates', this.carbohydrates),
+        this.createObject('sugars', this.sugars),
+        this.createObject('total_fats', this.fats),
+        this.createObject('saturated_fats', this.saturatedFats),
+        this.createObject('proteins', this.proteins),
+        this.createObject('salt', this.salt),
+        this.createObject('sodium', this.sodium),
+        this.createObject('calcium', this.calcium),
+        this.createObject('alcohol', this.alcohol),
+        this.createObject('fibers', this.fibers),
+      ];
+    },
+    createObject(name, values) {
+      let obj;
+      const firstObject = {
+        dataV: values,
+        dataPie: this.$i18n.t(name),
+        valueBox: {
+          text: '%data-bubble',
+          placement: 'bottom',
+          fontColor: 'black',
+          fontFamily: 'Arial',
+          fontWeight: 'bold',
+          fontSize: '10',
+        },
+      };
+
+      if (name === 'carbohydrates') obj = firstObject;
+      else {
+        obj = {
+          dataV: values,
+          dataPie: this.$i18n.t(name),
+        };
+      }
+
+      return obj;
+    },
     checkError(error, status) {
       if (error === 'internal_server_error' || error === 'meal_not_found') {
         this.errorMsgModal = error;
